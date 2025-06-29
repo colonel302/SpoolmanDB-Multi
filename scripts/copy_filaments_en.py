a5437decb7fb9cb92416204ad33f81c7137d950d
colonel302 <b.wuelfing@outlook.de>
2025-06-29 18:21:04 +0200

    Update copy_filaments_en.py

--- scripts/copy_filaments_en.py
+++ scripts/copy_filaments_en.py
@@ -2,8 +2,8 @@ import shutil
 import os
 
 def copy_filaments_json_to_en():
-    source = 'filaments.json'
-    target = 'filaments_en.json'
+    source = 'public/filaments.json'
+    target = 'public/filaments_en.json'
     if os.path.exists(source):
         shutil.copyfile(source, target)
         print(f"Kopiert {source} zu {target}")
